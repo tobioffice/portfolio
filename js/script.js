b2e14f1d7eeb1c9b5651135a714465e41c0f8ec7
Your Name <you@example.com>
2024-01-16 14:03:41 +0530

    changed nav

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const navTElement = document.querySelector(".menu_icon");
-const flexContainer = document.querySelector(".nav-t");
+const flexContainer = document.querySelector(".hide");
 const listItem = document.querySelectorAll(".li");
 navTElement.addEventListener("click", () => {
-  flexContainer.classList.toggle("nav-t_open");
+  flexContainer.classList.toggle("unhide");
 });
 listItem.forEach((liItem) => {
   liItem.addEventListener("click", () => {
