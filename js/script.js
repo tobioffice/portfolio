4abc6a89cb87a3d672c0a5d45f5639255237106b
Your Name <you@example.com>
2024-01-31 13:11:33 +0530

    nav bar change

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 const navTElement = document.querySelector(".menu_icon");
 const flexContainer = document.querySelector(".hide");
-const listItem = document.querySelectorAll(".li");
+const listItem = document.querySelectorAll(".li_mobile");
 navTElement.addEventListener("click", () => {
   flexContainer.classList.toggle("unhide");
 });
 listItem.forEach((liItem) => {
   liItem.addEventListener("click", () => {
-    flexContainer.classList.remove("nav-t_open");
+    flexContainer.classList.remove("unhide");
   });
 });
